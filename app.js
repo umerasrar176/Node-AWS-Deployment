2801128180ea50c345099fdb0d806ca0db882519
Umer Hayat <umerasrar17676@gmail.com>
2022-10-31 03:32:54 +0500

    changes the app.js

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ app.get("/new", function (req, res) {
   res.send("New also worked");
 });
 
+app.get("/test", function (req, res) {
+  res.send("Testing the Site on deployment");
+});
+
 app.listen(process.env.PORT || 5000);
 module.exports = app;
