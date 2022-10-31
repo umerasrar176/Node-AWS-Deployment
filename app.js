895ba6a6a6bce3a24723687aa71e7e89c092be94
Umer Hayat <umerasrar17676@gmail.com>
2022-11-01 04:14:20 +0500

    added upload object code

--- app.js
+++ app.js
@@ -12,5 +12,21 @@ app.get("/test", function (req, res) {
   res.send("Testing the Site on deployment");
 });
 
+const options = {
+  url: '/upload',
+  json: true,
+  body: {
+      name: 'John Doe',
+      job: 'Content Writer'
+  }
+};
+// post the image direclty to the s3 bucket
+app.post(options, (error, response, body)=> {
+  if (!error && response.statusCode == 200) {
+      console.log(body);
+      console.log(response.statusCode);
+  }
+})
+
 app.listen(process.env.PORT || 5000);
 module.exports = app;
